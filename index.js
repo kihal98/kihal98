e724c9420fe270804ae80b8f06a63097dd9dbb58
kihal98 <kihal0987@gmail.com>
2025-02-24 00:02:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
-const { Client, version } = require('discord.js');
-const { 
-    token, 
-    serverID, 
-    roleID, 
-    interval 
-} = require('./config.json')
-const bot = new Client();
+require("dotenv").config();
 
-bot.on("ready", async() => {
-    console.log(`[ Client ] ${bot.user.tag} Is Now Online`);
+// Keep Alive System
+const keepAlive = require("./keep_alive.js");
+keepAlive();
 
-    let guild = bot.guilds.cache.get(serverID) 
-    if(!guild) throw `[ Error ] Didn't Find Any Server : ${serverID}` 
+// Express Setup
+const express = require("express");
+const app = express();
 
-    let role = guild.roles.cache.find(u => u.id === roleID) 
-    if(!role) throw `[ Error ] Didn't Find Any Role, Server Name: ${guild.name}` 
-    
-    if(interval < 60000) console.log(`\n[!!!] Enjoy Your Rainbow Roles`) 
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
 
-    setInterval(() => {
-        role.edit({color: 'RANDOM'}).catch(err => console.log(`[ Error ] An error occurred during the role change.`));
-    }, interval)
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
-    bot.user.setPresence({
-        status: 'dnd',
-        activity: {
-            name: 'Roles Color Changer',
-            type: 'WATCHING',
-        }
-    })
-})
+// Discord Bot Setup
+const { Client, Intents } = require("discord.js");
 
+const bot = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
+});
 
-bot.login(token)
+bot.on("ready", async () => {
+  console.log(`[ Client ] ${bot.user.tag} Is Now Online`);
+
+  let guild = bot.guilds.cache.get(process.env.SERVER_ID);
+  if (!guild) throw `[ Error ] Didn't Find Any Server: ${process.env.SERVER_ID}`;
+
+  let role = guild.roles.cache.find((r) => r.id === process.env.ROLE_ID);
+  if (!role) throw `[ Error ] Didn't Find Any Role in ${guild.name}`;
+
+  if (process.env.INTERVAL < 6000) console.log("!!! Enjoy Your Rainbow Roles");
+
+  setInterval(() => {
+    role
+      .edit({ color: Math.floor(Math.random() * 16777215) })
+      .catch((err) => console.log(`[ Error ] An error occurred during the role change:`, err));
+  }, parseInt(process.env.INTERVAL));
+
+  bot.user.setPresence({
+    status: "dnd",
+    activity: {
+      name: "Roles Color Changer",
+      type: "WATCHING",
+    },
+  });
+});
+
+bot.login(process.env.BOT_TOKEN);
 
 
